445a06966ab277fb949bf21abb3f495c39751392
nschickm <nschickm@htl-steyr.ac.at>
2022-12-21 10:10:53 +0100

    Chess pieces

--- src/main/java/com/example/chess/model/King.java
+++ src/main/java/com/example/chess/model/King.java
@@ -1,15 +1,74 @@
 package com.example.chess.model;
 
-public class King extends Piece{
+import java.util.Objects;
+
+public class King extends Piece {
     public King(int x, int y, String color) {
         super(x, y, color);
         name = "king";
     }
 
     @Override
-    public boolean checkMove(Piece[][] board, int targetX, int targetY) {
-        boolean r = false;
+    public Boolean[][] getPossibleMoves(Piece[][] board) {
+        Boolean[][] possibleMoves = new Boolean[MAX_X][MAX_Y];
+        for (int i = 0; i < MAX_X; i++) {
+            for (int j = 0; j < MAX_Y; j++) {
+                possibleMoves[i][j] = false;
+            }
+        }
 
-        return r;
+
+        if (board[x++][y] == null) {
+            possibleMoves[x++][y] = true;
+        } else if (!Objects.equals(board[x++][y].color, color)) {
+            possibleMoves[x++][y] = true;
+        }
+
+        if (board[x--][y] == null) {
+            possibleMoves[x--][y] = true;
+        } else if (!Objects.equals(board[x--][y].color, color)) {
+            possibleMoves[x--][y] = true;
+        }
+
+
+        if (board[x++][y++] == null) {
+            possibleMoves[x++][y++] = true;
+        } else if (!Objects.equals(board[x++][y++].color, color)) {
+            possibleMoves[x++][y++] = true;
+        }
+
+        if (board[x--][y++] == null) {
+            possibleMoves[x--][y++] = true;
+        } else if (!Objects.equals(board[x--][y++].color, color)) {
+            possibleMoves[x--][y++] = true;
+        }
+
+        if (board[x][y++] == null) {
+            possibleMoves[x][y++] = true;
+        } else if (!Objects.equals(board[x][y++].color, color)) {
+            possibleMoves[x][y++] = true;
+        }
+
+
+        if (board[x++][y--] == null) {
+            possibleMoves[x++][y--] = true;
+        } else if (!Objects.equals(board[x++][y--].color, color)) {
+            possibleMoves[x++][y--] = true;
+        }
+
+        if (board[x--][y--] == null) {
+            possibleMoves[x--][y--] = true;
+        } else if (!Objects.equals(board[x--][y--].color, color)) {
+            possibleMoves[x--][y--] = true;
+        }
+
+        if (board[x][y--] == null) {
+            possibleMoves[x][y--] = true;
+        } else if (!Objects.equals(board[x][y--].color, color)) {
+            possibleMoves[x][y--] = true;
+        }
+
+
+        return possibleMoves;
     }
 }
